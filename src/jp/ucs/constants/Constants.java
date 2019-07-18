1ce2571ed8f278b193650bd4b4117dade269b886
hryk.hoshi <hryk.hoshi@PNO-1902-29.ucs.local>
2019-07-18 13:49:10 +0900

    コメント

--- src/jp/ucs/constants/Constants.java
+++ src/jp/ucs/constants/Constants.java
@@ -8,6 +8,7 @@ public final class Constants {
 
 	//定数を定義する
 	public static final String NEW_LINE = "\n";
+	public static final String NEW_LINE2 = "\r\n";
 	public static final String changeEmp = "/WEB-INF/jsp//changeEmp.jsp";
 	public static final String changeCheck = "/WEB-INF/jsp/changeCheck.jsp";
 	public static final String changeComp =  "/WEB-INF/jsp/changeComp.jsp";
