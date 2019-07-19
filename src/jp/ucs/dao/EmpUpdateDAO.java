ab5c508d386af43152b85c4f123c497288a173ad
knk.konishi <knk.konishi@PNO-1902-08.ucs.local>
2019-07-19 09:06:05 +0900

    aa

--- src/jp/ucs/dao/EmpUpdateDAO.java
+++ src/jp/ucs/dao/EmpUpdateDAO.java
@@ -1,11 +1,11 @@
 package jp.ucs.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import jp.ucs.bean.EmployeeBean;
+import jp.ucs.constants.MessageConstants;
 import jp.ucs.exception.HrsmUcsDBException;
 
 /**
@@ -20,37 +20,38 @@ import jp.ucs.exception.HrsmUcsDBException;
 public class EmpUpdateDAO extends BaseDAO{
 
 	public boolean empUpdate(EmployeeBean afterEmp) throws HrsmUcsDBException{
-		try (Connection  conn = DriverManager.getConnection(DB_URL, DB_ID, PWD)){
-
-			StringBuilder sb = new StringBuilder();
-
+		try (Connection  conn = getConnection()){
 			//SQL文の準備
-			sb.append("UPDATE employee ");
-			sb.append( "SET emp_id = ?, emp_name = ?, ruby = ?, ");
-			sb.append("pass = ?, entry_date = ?, dept = ? ");
-			sb.append("WHERE property_id = ? AND serial_id = ?;");
+			StringBuilder sb = new StringBuilder();
+           sb.append("UPDATE employee ");
+           sb.append("SET property_id = ?, serial_id = ?, ");
+           sb.append("emp_name = = ?, ruby = ?, ");
+           sb.append("pass = ?, entry_date = ?, dept_id = ? ");
+           sb.append("WHERE property_id = ? AND serial_id = ?;");
 
-			//SQL文の実行
-			PreparedStatement pStmt = conn.prepareStatement(sb.toString());
-			String id = afterEmp.getEmpId();
-			String propertyId = id.substring(0, 4);
-			String serialId = id.substring(4, 8);
-			pStmt.setString(1, propertyId);
-			pStmt.setString(2, serialId);
-			pStmt.setString(3, afterEmp.getEmpName());
-			pStmt.setString(4, afterEmp.getRuby());
-			pStmt.setString(5, afterEmp.getPass());
-			pStmt.setString(6, afterEmp.getEntryDate());
-			pStmt.setString(7, afterEmp.getDept().getDeptId());
-			pStmt.setString(8, propertyId);
-			pStmt.setString(9, serialId);
-
-			pStmt.executeUpdate();
+            //SQL文の実行
+            PreparedStatement pStmt = conn.prepareStatement(sb.toString());
+            String id = afterEmp.getEmpId();
+            String propertyId = id.substring(0, 4);
+            String serialId = id.substring(4, 8);
+            pStmt.setString(1, propertyId);
+            pStmt.setString(2, serialId);
+            pStmt.setString(3, afterEmp.getEmpName());
+            pStmt.setString(4, afterEmp.getRuby());
+            pStmt.setString(5, afterEmp.getPass());
+            pStmt.setString(6, afterEmp.getEntryDate());
+            pStmt.setString(7, afterEmp.getDept().getDeptId());
+            pStmt.setString(8, propertyId);
+            pStmt.setString(9, serialId);
 
 
-		}catch(SQLException e){
-			e.printStackTrace();
-			throw new HrsmUcsDBException();
-		}return true;
-}
-}
+            pStmt.executeUpdate();
+
+            return true;
+        }catch(SQLException e){
+            throw new HrsmUcsDBException(MessageConstants.DB_ERR01);
+        }
+    }
+		}
+
+
